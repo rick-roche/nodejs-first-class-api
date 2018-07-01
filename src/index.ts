c2f75936c7d81acba705699a819d528106da441c
Rick Roche <richard.j.roche@gmail.com>
2018-07-01 20:13:22 +0200

    feat: add winston for logging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 
+import * as logger from 'winston';
+
 const app = new Koa();
 const router = new Router();
+
+logger.configure({
+    level: 'info',
+    format: logger.format.combine(
+        logger.format.timestamp(),
+        logger.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+    ),
+    transports: [
+        new logger.transports.Console({
+            format: logger.format.simple(),
+        }),
+    ],
+});
 
 router.get('/*', async ctx => {
     ctx.body = 'Hello World!';
@@ -12,4 +27,4 @@ app.use(router.routes());
 
 app.listen(3000);
 
-// console.log('Server running on port 3000');
+logger.info('Koa application is up and running on port 3000');
