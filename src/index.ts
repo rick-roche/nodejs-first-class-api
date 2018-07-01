5ccdfb2d61fbf2c8b5f580e0063a5fc72abae40f
Rick Roche <richard.j.roche@gmail.com>
2018-07-01 20:46:28 +0200

    chore: configure some koa middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,45 @@
 import * as Koa from 'koa';
+import * as compress from 'koa-compress';
+import * as logger from 'koa-logger';
+import * as responseTime from 'koa-response-time';
 import * as Router from 'koa-router';
-
-import * as logger from 'winston';
+import * as winston from 'winston';
 
 const app = new Koa();
 const router = new Router();
 
-logger.configure({
-    level: 'info',
-    format: logger.format.combine(
-        logger.format.timestamp(),
-        logger.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+winston.configure({
+    level: 'debug',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
     ),
     transports: [
-        new logger.transports.Console({
-            format: logger.format.simple(),
+        new winston.transports.Console({
+            format: winston.format.simple(),
         }),
     ],
 });
 
+// app.use(async (ctx, next) => {
+//     // Log the request to the console
+//     winston.debug(`Url: ${ctx.url}`);
+//     // Pass the request to the next middleware function
+//     await next();
+// });
+
+// logging
+app.use(logger());
+// x-response-time
+app.use(responseTime());
+// compression
+app.use(compress());
+
 router.get('/*', async ctx => {
     ctx.body = 'Hello World!';
 });
-
 app.use(router.routes());
 
 app.listen(3000);
 
-logger.info('Koa application is up and running on port 3000');
+winston.info('Koa application is up and running on port 3000');
