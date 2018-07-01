3d3578a866bc6d38fcd01d7308055ca9622353b3
Rick Roche <richard.j.roche@gmail.com>
2018-07-01 19:25:34 +0200

    chore: linter fixes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as Router from 'koa-router';
 const app = new Koa();
 const router = new Router();
 
-router.get('/*', async (ctx) => {
+router.get('/*', async ctx => {
     ctx.body = 'Hello World!';
 });
 
